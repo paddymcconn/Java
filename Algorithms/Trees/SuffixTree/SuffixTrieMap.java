34afe4f7e9048f3c5e49b300515ddaa673d14445
paddymcconn <paddymcconn1280@hotmail.com>
2018-06-24 19:02:58 +0100

    Add files via upload

---
+++ Algorithms/Trees/SuffixTree/SuffixTrieMap.java
@@ -0,0 +1,29 @@
+package trees.suffixtrie;
+
+import java.util.List;
+
+public class SuffixTrieMap
+{
+
+    TrieNodeMap root = new TrieNodeMap();
+
+    public SuffixTrieMap(String text)
+    {
+        root.insertSuffix(text);
+    }
+
+    public List<Integer> search(String pattern)
+    {
+        return root.search(pattern);
+    }
+
+    public boolean isSuffix(String pattern)
+    {
+        return root.isSuffix(pattern);
+    }
+
+    public boolean isSubstring(String pattern)
+    {
+        return root.isSubstring(pattern);
+    }
+}
