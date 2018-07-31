25ae78b08d2a35645df392db597bbc4ab34e106e
paddymcconn <paddymcconn1280@hotmail.com>
2018-07-31 17:58:30 +0100

    Add files via upload

--- HackerRank/Java/Exception_Handling/Java_Try_Catch.java
+++ HackerRank/Java/Exception_Handling/Java_Try_Catch.java
@@ -1,1 +1,33 @@
+package Exception_Handling;
+import java.util.*;
 
+
+public class Java_Try_Catch
+{
+	public static void main(String[] args)
+    {
+		try
+		{
+			Scanner Sc = new Scanner(System.in);
+			int x = Sc.nextInt();
+			int y = Sc.nextInt();
+			Sc.close();
+			if(y==0)
+			{
+				throw  new ArithmeticException("/ by zero");
+            }
+            else
+            {
+            	System.out.println(x/y);
+            }
+        }
+		catch(InputMismatchException e)
+        {
+			System.out.println(e.getClass().getName());
+        }
+        catch(ArithmeticException e)
+        {
+        	System.out.println(e);
+        }
+    }
+}
